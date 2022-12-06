379f522461e997c307c5f1e1f310fcda31d94402
Jake Hansen <JakeMHansen95@gmail.com>
2022-12-05 23:40:02 -0600

    Cleanup

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -12,18 +12,16 @@ fn main() {
 }
 
 fn part1(input: &str) -> R<usize> {
+    const CONSECUTIVE_CHARS: usize = 4;
+
     let mut iter = peek_nth(enumerate(input.chars()));
     while let Some((i, current)) = iter.next() {
-        let next1 = iter.peek_nth(0).unwrap().1;
-        let next2 = iter.peek_nth(1).unwrap().1;
-        let next3 = iter.peek_nth(2).unwrap().1;
-        let a = vec![current, next1, next2, next3]
-            .into_iter()
-            .unique()
-            .collect::<Vec<_>>();
-
-        if a.len() == 4 {
-            let result = i + 4;
+        let mut chars = vec![current];
+        for index in 0..CONSECUTIVE_CHARS - 1 {
+            chars.push(iter.peek_nth(index).unwrap().1);
+        }
+        if chars.into_iter().unique().count() == CONSECUTIVE_CHARS {
+            let result = i + CONSECUTIVE_CHARS;
             return Ok(result);
         }
     }
@@ -31,30 +29,16 @@ fn part1(input: &str) -> R<usize> {
 }
 
 fn part2(input: &str) -> R<usize> {
+    const CONSECUTIVE_CHARS: usize = 14;
+
     let mut iter = peek_nth(enumerate(input.chars()));
     while let Some((i, current)) = iter.next() {
-        let next1 = iter.peek_nth(0).unwrap().1;
-        let next2 = iter.peek_nth(1).unwrap().1;
-        let next3 = iter.peek_nth(2).unwrap().1;
-        let next4 = iter.peek_nth(3).unwrap().1;
-        let next5 = iter.peek_nth(4).unwrap().1;
-        let next6 = iter.peek_nth(5).unwrap().1;
-        let next7 = iter.peek_nth(6).unwrap().1;
-        let next8 = iter.peek_nth(7).unwrap().1;
-        let next9 = iter.peek_nth(8).unwrap().1;
-        let next10 = iter.peek_nth(9).unwrap().1;
-        let next11 = iter.peek_nth(10).unwrap().1;
-        let next12 = iter.peek_nth(11).unwrap().1;
-        let next13 = iter.peek_nth(12).unwrap().1;
-        let a = vec![
-            current, next1, next2, next3, next4, next5, next6, next7, next8, next9, next10, next11, next12, next13,
-        ]
-        .into_iter()
-        .unique()
-        .collect::<Vec<_>>();
-
-        if a.len() == 14 {
-            let result = i + 14;
+        let mut chars = vec![current];
+        for index in 0..CONSECUTIVE_CHARS - 1 {
+            chars.push(iter.peek_nth(index).unwrap().1);
+        }
+        if chars.into_iter().unique().count() == CONSECUTIVE_CHARS {
+            let result = i + CONSECUTIVE_CHARS;
             return Ok(result);
         }
     }
