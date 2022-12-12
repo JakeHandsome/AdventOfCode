a0066c4a28c8062550a5fbf893326564dd297412
Jake Hansen <JakeMHansen95@gmail.com>
2022-12-12 15:44:52 -0600

    Add smart part 2 solution

---
+++ 2022/day12/README.md
@@ -0,0 +1,10 @@
+Part1: 370
+Time of Part 1 from_start, 757ms
+Part1: 370
+Time of Part 1 from_end, 80ms
+Part2: 363
+Time of Part 2 from_start, 20474ms
+Part2: 363
+Time of Part 2 from_end, 5922ms
+Part2: 363
+Time of Part 2 calc all and cache, 212ms
--- 2022/day12/src/main.rs
+++ 2022/day12/src/main.rs
@@ -21,6 +21,10 @@ fn main() {
         let _timer = Timer::new("Part 2 from_end");
         println!("Part2: {}", part2(&input, true).unwrap());
     }
+    {
+        let _timer = Timer::new("Part 2 calc all and cache");
+        println!("Part2: {}", part2_calc_all_and_cache(&input).unwrap());
+    }
 }
 
 struct Map {
@@ -129,6 +133,45 @@ impl Map {
         }
     }
 
+    fn calc_distance_from_end_memoize(&mut self, start: usize, end: usize) -> R<Vec<Tile>> {
+        let start_tile = Tile {
+            position: self.index_to_x_y(end),
+            height: b'z',
+            distance: 0,
+        };
+        let end = self.index_to_x_y(start);
+        let mut all_tiles = vec![start_tile];
+        let mut num_to_skip = 0;
+        loop {
+            let mut new_tiles: Vec<Tile> = vec![];
+            // only process the new tiles addec
+            for tile in &all_tiles[num_to_skip..all_tiles.len()] {
+                // Check the up down left right for each new tile
+                let up = (tile.position.0, tile.position.1 - 1);
+                let down = (tile.position.0, tile.position.1 + 1);
+                let left = (tile.position.0 - 1, tile.position.1);
+                let right = (tile.position.0 + 1, tile.position.1);
+                for position in [up, down, left, right] {
+                    if let Some(new_tile) = self.is_tile_new_and_navigable(position, &all_tiles, &new_tiles, tile, true)
+                    {
+                        new_tiles.push(new_tile);
+                    }
+                }
+            }
+            num_to_skip = new_tiles.len();
+            all_tiles.append(&mut new_tiles);
+            if num_to_skip == 0 {
+                // This point cannot reach the end
+                #[cfg(debug)]
+                println!("stuck! {:?}", all_tiles.first().unwrap().position);
+                return Ok(all_tiles);
+            }
+            if let Some(tile) = all_tiles.iter().find(|x| x.position == end) {
+                #[cfg(debug)]
+                println!("Found  {:?} = {}", all_tiles.first().unwrap().position, tile.distance);
+            }
+        }
+    }
     /// Determines if the new tile
     fn is_tile_new_and_navigable(
         &self,
@@ -163,6 +206,7 @@ impl Map {
     }
 }
 
+#[derive(Clone, Copy)]
 struct Tile {
     position: (isize, isize),
     height: u8,
@@ -263,6 +307,59 @@ fn part2(input: &str, from_end: bool) -> R<usize> {
     }
 }
 
+fn part2_calc_all_and_cache(input: &str) -> R<usize> {
+    let mut tiles: Vec<char> = vec![];
+    let mut width = None;
+    let height = input.lines().count();
+    for line in input.lines() {
+        if width.is_none() {
+            width = Some(line.len());
+        }
+        for c in line.chars() {
+            tiles.push(c);
+        }
+    }
+    let end = tiles.iter().position(|x| *x == 'E').unwrap();
+    let tiles: Vec<u8> = tiles
+        .into_iter()
+        .map(|x| match x {
+            'S' => b'a',
+            'E' => b'z',
+            _ => x as u8,
+        })
+        .collect();
+    // Get all the start indexes
+    let starts = tiles
+        .iter()
+        .enumerate()
+        .filter(|(_, c)| **c == b'a')
+        .map(|(i, _)| i.to_owned())
+        .collect::<Vec<_>>();
+    let mut min = usize::MAX;
+
+    let mut map = Map {
+        tiles: tiles.clone(),
+        width: width.unwrap(),
+        height,
+    };
+    let tiles = map.calc_distance_from_end_memoize(0, end).unwrap();
+    for start in starts {
+        match tiles
+            .iter()
+            .find(|x| map.index_to_x_y(start) == x.position)
+            .map(|x| x.distance)
+        {
+            Some(distance) => {
+                if distance < min {
+                    min = distance;
+                }
+            }
+            None => continue,
+        }
+    }
+
+    Ok(min)
+}
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -273,10 +370,10 @@ acctuvwj
 abdefghi"#;
     #[test]
     fn p1_test() {
-        assert_eq!(part1(SAMPLE1).unwrap(), 31);
+        assert_eq!(part1(SAMPLE1, true).unwrap(), 31);
     }
     #[test]
     fn p2_test() {
-        assert_eq!(part2(SAMPLE1).unwrap(), 29);
+        assert_eq!(part2(SAMPLE1, true).unwrap(), 29);
     }
 }
