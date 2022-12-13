6d0410ba92893364ac51ae27025b12b761f2a8cc
Jake Hansen <JakeMHansen95@gmail.com>
2022-12-13 02:10:26 -0600

    Multiple files

--- 2022/day13/src/main.rs
+++ 2022/day13/src/main.rs
@@ -1,3 +1,6 @@
+mod packet;
+
+use packet::Packet;
 use std::{fmt::Display, vec};
 
 use common::*;
@@ -14,103 +17,8 @@ fn main() {
     }
 }
 
-#[derive(Debug, Eq, PartialEq, Clone)]
-enum Packet {
-    Value(usize),
-    Array(Vec<Packet>),
-    Empty,
-}
-
-impl Packet {
-    fn new(input: String) -> Self {
-        if input.starts_with('[') {
-            let mut split = input[1..input.len() - 1].split(',').peekable();
-            let mut elements: Vec<Packet> = vec![];
-            let mut current = "".to_string();
-            while split.peek().is_some() || current != *"" {
-                if current.is_empty() {
-                    current = split.next().unwrap().into()
-                }
-                let numopen = current.chars().filter(|c| *c == '[').count();
-                let numclose = current.chars().filter(|c| *c == ']').count();
-                if (current.contains('[')
-                    && current.contains(']')
-                        //Make sure the number of ] matches number of [
-                && numopen == numclose)
-                    || !current.contains('[') && !current.contains(']')
-                {
-                    // This is an element or a value
-                    elements.push(Packet::new(current));
-                    current = "".into();
-                } else if numopen != numclose {
-                    // Combine with next string
-                    current = current.to_string() + "," + split.next().unwrap();
-                }
-            }
-            Array(elements)
-        } else {
-            match input.parse() {
-                Ok(x) => Value(x),
-                Err(_) => Empty,
-            }
-        }
-    }
-}
-
-impl PartialOrd for Packet {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for Packet {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        let left = self;
-        let right = other;
-        use std::cmp::Ordering::*;
-        match (left.to_owned(), right.to_owned()) {
-            // Compare values
-            (Value(left), Value(right)) => left.cmp(&right),
-            // Convert value into array an compare
-            (Value(left), Array(right)) => Array(vec![Value(left)]).cmp(&Array(right)),
-            (Array(left), Value(right)) => Array(left).cmp(&Array(vec![Value(right)])),
-            // Right side end early
-            (Array(_), Empty) | (Value(_), Empty) => Greater,
-            // Left side end early
-            (Empty, Value(_)) | (Empty, Array(_)) => Less,
-            (Empty, Empty) => Equal,
-            (Array(left), Array(right)) => {
-                let mut left = left.into_iter();
-                let mut right = right.into_iter();
-                match (left.next(), right.next()) {
-                    (None, None) => Equal,
-                    (None, Some(x)) => Empty.cmp(&x),
-                    (Some(x), None) => x.cmp(&Empty),
-                    (Some(l), Some(r)) => match l.cmp(&r) {
-                        Equal => Array(left.collect()).cmp(&Array(right.collect())),
-                        x => x,
-                    },
-                }
-            }
-        }
-    }
-}
-
-impl Display for Packet {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Value(x) => f.write_fmt(format_args!("{}", x)),
-            Array(x) => f.write_fmt(format_args!(
-                "[{}]",
-                x.iter().map(|y| y.to_string()).collect::<Vec<_>>().join(",")
-            )),
-            Empty => Ok(()),
-        }
-    }
-}
-
 #[derive(Debug)]
-struct Pair {
+pub struct Pair {
     left: Packet,
     right: Packet,
 }
@@ -129,8 +37,6 @@ impl Display for Pair {
         f.write_fmt(format_args!("{:?}\n{:?}\n", self.left, self.right))
     }
 }
-
-use Packet::*;
 
 fn part1(input: &str) -> R<usize> {
     let mut result = 0usize;
@@ -162,7 +68,7 @@ fn part2(input: &str) -> R<usize> {
     let result = packets
         .into_iter()
         .enumerate()
-        .filter(|x| x.1.to_string() == "[[2]]" || x.1.to_string() == "[[6]]")
+        .filter(|x| format!("{}", x.1) == "[[2]]" || format!("{}", x.1) == "[[6]]")
         .map(|x| x.0 + 1)
         .product();
     Ok(result)
---
+++ 2022/day13/src/packet.rs
@@ -0,0 +1,97 @@
+#[derive(Debug, Eq, PartialEq, Clone)]
+pub enum Packet {
+    Value(usize),
+    Array(Vec<Packet>),
+    Empty,
+}
+use std::fmt::Display;
+
+use Packet::*;
+
+impl Packet {
+    pub fn new(input: String) -> Self {
+        if input.starts_with('[') {
+            let mut split = input[1..input.len() - 1].split(',').peekable();
+            let mut elements: Vec<Packet> = vec![];
+            let mut current = "".to_string();
+            while split.peek().is_some() || current != *"" {
+                if current.is_empty() {
+                    current = split.next().unwrap().into()
+                }
+                let numopen = current.chars().filter(|c| *c == '[').count();
+                let numclose = current.chars().filter(|c| *c == ']').count();
+                if (current.contains('[')
+                    && current.contains(']')
+                        //Make sure the number of ] matches number of [
+                && numopen == numclose)
+                    || !current.contains('[') && !current.contains(']')
+                {
+                    // This is an element or a value
+                    elements.push(Packet::new(current));
+                    current = "".into();
+                } else if numopen != numclose {
+                    // Combine with next string
+                    current = current.to_string() + "," + split.next().unwrap();
+                }
+            }
+            Array(elements)
+        } else {
+            match input.parse() {
+                Ok(x) => Value(x),
+                Err(_) => Empty,
+            }
+        }
+    }
+}
+
+impl PartialOrd for Packet {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Packet {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        let left = self;
+        let right = other;
+        use std::cmp::Ordering::*;
+        match (left.to_owned(), right.to_owned()) {
+            // Compare values
+            (Value(left), Value(right)) => left.cmp(&right),
+            // Convert value into array an compare
+            (Value(left), Array(right)) => Array(vec![Value(left)]).cmp(&Array(right)),
+            (Array(left), Value(right)) => Array(left).cmp(&Array(vec![Value(right)])),
+            // Right side end early
+            (Array(_), Empty) | (Value(_), Empty) => Greater,
+            // Left side end early
+            (Empty, Value(_)) | (Empty, Array(_)) => Less,
+            (Empty, Empty) => Equal,
+            (Array(left), Array(right)) => {
+                let mut left = left.into_iter();
+                let mut right = right.into_iter();
+                match (left.next(), right.next()) {
+                    (None, None) => Equal,
+                    (None, Some(x)) => Empty.cmp(&x),
+                    (Some(x), None) => x.cmp(&Empty),
+                    (Some(l), Some(r)) => match l.cmp(&r) {
+                        Equal => Array(left.collect()).cmp(&Array(right.collect())),
+                        x => x,
+                    },
+                }
+            }
+        }
+    }
+}
+
+impl Display for Packet {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Value(x) => f.write_fmt(format_args!("{}", x)),
+            Array(x) => f.write_fmt(format_args!(
+                "[{}]",
+                x.iter().map(|y| y.to_string()).collect::<Vec<_>>().join(",")
+            )),
+            Empty => Ok(()),
+        }
+    }
+}
