6d6d2d2d11951d37431fe69e7c3af672161829ef
Jake Hansen <JakeMHansen95@gmail.com>
2022-12-14 22:58:03 -0600

    Remove map

---
+++ 2022/day15/src/main.rs
@@ -0,0 +1,35 @@
+use common::*;
+
+fn main() {
+    let input = read_input_file_for_project_as_string!();
+    {
+        let _timer = Timer::new("Part 1");
+        println!("Part1: {}", part1(&input).unwrap());
+    }
+    {
+        let _timer = Timer::new("Part 2");
+        println!("Part2: {}", part2(&input).unwrap());
+    }
+}
+
+fn part1(input: &str) -> R<usize> {
+    Err(Box::new(AdventOfCodeError::new("Not implemented")))
+}
+
+fn part2(input: &str) -> R<usize> {
+    Err(Box::new(AdventOfCodeError::new("Not implemented")))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const SAMPLE1: &str = r#""#;
+    #[test]
+    fn p1_test() {
+        assert_eq!(part1(SAMPLE1).unwrap(), 13140);
+    }
+    #[test]
+    fn p2_test() {
+        assert_eq!(part2(SAMPLE1).unwrap(), 0);
+    }
+}
