3c27ac4fc0dccc788c1f7e6bdbc043a5b0b257e0
Jake Hansen <JakeMHansen95@gmail.com>
2023-12-12 08:07:27 -0600

    day12 p1

---
+++ 2023/day12/src/main.rs
@@ -0,0 +1,172 @@
+use common::*;
+
+fn main() {
+    let input = read_input_file_for_project_as_string!();
+    {
+        let _timer = Timer::new("Part 1");
+        println!("Part1: {}", part1(&input).unwrap());
+    }
+    {
+        let _timer = Timer::new("Part 2");
+        println!("Part2: {}", part2(&input).unwrap());
+    }
+}
+
+fn part1(input: &str) -> anyhow::Result<usize> {
+    Ok(input.lines().map(solve_line).sum())
+}
+
+fn solve_line(input: &str) -> usize {
+    let mut split = input.split(' ');
+    let puzzle = split.next().unwrap();
+    let num_unknown = puzzle.chars().filter(|x| *x == '?').count();
+    let key = split
+        .next()
+        .unwrap()
+        .split(',')
+        .map(|x| x.parse::<usize>().unwrap())
+        .collect_vec();
+    let mut solutions = 0;
+    for i in 0..=(2usize.pow(num_unknown as u32) - 1) {
+        let mut puzzle = String::from(puzzle);
+        for j in (0..num_unknown).rev() {
+            if (i >> j) & 1 == 1 {
+                puzzle = puzzle.replacen('?', "#", 1);
+            } else {
+                puzzle = puzzle.replacen('?', ".", 1);
+            }
+        }
+        let a = is_solution(puzzle.as_str(), &key);
+        if a {
+            solutions += 1;
+        }
+    }
+
+    solutions
+}
+
+fn is_solution(as_str: &str, key: &[usize]) -> bool {
+    let mut last_char = '.';
+    let mut key_index = 0;
+    let mut current_count = 0;
+    // Make sure every sequence ends with '.'
+    for char in as_str.chars().chain(['.'].into_iter()) {
+        match (last_char, char) {
+            // Empty space to empty space do nothing
+            ('.', '.') => (),
+            // Filled to empty,
+            ('#', '.') => {
+                // If the key exists and matches, continue
+                if let Some(count) = key.get(key_index) {
+                    if current_count == *count {
+                        current_count = 0;
+                        key_index += 1;
+                    } else {
+                        return false;
+                    }
+                } else {
+                    // If a match fais return false
+                    return false;
+                }
+            }
+            // next is filled, increment current count, make sure it doesn't excede max count
+            (_, '#') => {
+                current_count += 1;
+                if let Some(count) = key.get(key_index) {
+                    if current_count > *count {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
+            _ => unreachable!(),
+        }
+        last_char = char;
+    }
+    // Make sure we matched all keys
+    key_index >= key.len()
+}
+
+fn part2(input: &str) -> anyhow::Result<usize> {
+    Ok(input.lines().map(|x| solve_line(&unfold_line(x))).sum())
+}
+
+fn unfold_line(input: &str) -> String {
+    let mut split = input.split(' ');
+    let puzzle = split.next().unwrap();
+    let key = split.next().unwrap();
+    format!("{puzzle}?{puzzle}?{puzzle}?{puzzle}?{puzzle} {key},{key},{key},{key},{key}")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const SAMPLE1: &str = r#"???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1"#;
+    #[test]
+    fn p1_test() {
+        assert_eq!(part1(SAMPLE1).unwrap(), 21);
+    }
+    #[test]
+    fn p1_testl1() {
+        assert_eq!(solve_line(SAMPLE1.lines().nth(0).unwrap()), 1);
+    }
+    #[test]
+    fn p1_testl2() {
+        assert_eq!(solve_line(SAMPLE1.lines().nth(1).unwrap()), 4);
+    }
+    #[test]
+    fn p1_testl3() {
+        assert_eq!(solve_line(SAMPLE1.lines().nth(2).unwrap()), 1);
+    }
+    #[test]
+    fn p1_testl4() {
+        assert_eq!(solve_line(SAMPLE1.lines().nth(3).unwrap()), 1);
+    }
+    #[test]
+    fn p1_testl5() {
+        assert_eq!(solve_line(SAMPLE1.lines().nth(4).unwrap()), 4);
+    }
+    #[test]
+    fn p1_testl6() {
+        assert_eq!(solve_line(SAMPLE1.lines().nth(5).unwrap()), 10);
+    }
+    //#[test]
+    fn p2_test() {
+        assert_eq!(part2(SAMPLE1).unwrap(), 0);
+    }
+    #[test]
+    fn p2_testl1() {
+        let a = unfold_line(SAMPLE1.lines().nth(0).unwrap());
+        assert_eq!(
+            &a,
+            "???.###????.###????.###????.###????.### 1,1,3,1,1,3,1,1,3,1,1,3,1,1,3"
+        );
+        assert_eq!(solve_line(&a), 1);
+    }
+    #[test]
+    fn p2_testl2() {
+        assert_eq!(solve_line(&unfold_line(SAMPLE1.lines().nth(1).unwrap())), 4);
+    }
+    #[test]
+    fn p2_testl3() {
+        assert_eq!(solve_line(&unfold_line(SAMPLE1.lines().nth(2).unwrap())), 1);
+    }
+    #[test]
+    fn p2_testl4() {
+        assert_eq!(solve_line(&unfold_line(SAMPLE1.lines().nth(3).unwrap())), 1);
+    }
+    #[test]
+    fn p2_testl5() {
+        assert_eq!(solve_line(&unfold_line(SAMPLE1.lines().nth(4).unwrap())), 4);
+    }
+    #[test]
+    fn p2_testl6() {
+        assert_eq!(solve_line(&unfold_line(SAMPLE1.lines().nth(5).unwrap())), 10);
+    }
+}
